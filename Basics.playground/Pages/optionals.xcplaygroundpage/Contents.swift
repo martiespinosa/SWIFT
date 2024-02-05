9996395c137ab673ff9efeb095b81cdf30550b16
marti <martiespinosa99@gmail.com>
2024-02-05 20:38:14 +0100

    optional variables

---
+++ Basics.playground/Pages/optionals.xcplaygroundpage/Contents.swift
@@ -0,0 +1,33 @@
+import Foundation
+
+/*
+ We use "?" after the type to declare an optional variable.
+ And we use "??", "if-let" or "guard-let" statment to unwrap that optional
+ */
+
+var userIsPremium: Bool? = nil
+
+// Unwrapped with "??"
+func checkIfUserIsPremium() -> Bool {
+    return userIsPremium ?? false
+}
+
+// Unwrapped with "if-let" statment
+func checkIfUserIsPremium2() -> Bool {
+    if let userIsPremium {
+        return userIsPremium
+    }
+    return false
+}
+
+// Unwrapped with "guard-let" statment
+func checkIfUserIsPremium3() -> Bool {
+    guard let userIsPremium else {
+        return false
+    }
+    return userIsPremium
+}
+
+print(checkIfUserIsPremium())
+print(checkIfUserIsPremium2())
+print(checkIfUserIsPremium3())
